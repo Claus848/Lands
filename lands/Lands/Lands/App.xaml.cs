893f18683c76fbf83980c66a166bda501c73fe84
Clau <clausandrade97@gmail.com>
2020-03-09 21:56:50 -0600

    camios de  la claus

---
+++ lands/Lands/Lands/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Lands
+{
+    using Views;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+    public partial class App : Application
+    {
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage(new LoginPage());
+        }
+        #endregion
+        #region methods
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+        #endregion
+    }
+}
---
+++ lands/Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infrastructure
+{
+    using ViewModels;
+    class InstanceLocator
+    {
+        #region
+        public MainViewModel Main { get; set; }
+        #endregion
+        #region constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+
+        }
+        #endregion
+    }
+}
